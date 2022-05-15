c88a80e10d428abca454c10b5daf71f40e4567a6
seung mi <tmdal9953@gmail.com>
2022-05-15 22:24:32 +0900

    success without proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@ import React, {useEffect} from 'react';
 import axios from "axios";
 
 function App() {
+  // proxy 사용 x
   const getApi = async () => {
-    axios.get("/api")
+    axios.get("http://localhost:5000/api")
       .then(
         (res) => console.log(res.data)
       );
   };
+
+  // proxy 사용 o - 안됨
+  // const getApi = async () => {
+  //   axios.get("/api")
+  //     .then(
+  //       (res) => console.log(res.data)
+  //     );
+  // };
 
   useEffect(() => {
     getApi();
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-import express from "express";
 import api from "./api";
 
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
+
+app.use(express.json());
+app.use(cors({
+    exposedHeaders: ['authorization'],
+}));
 
 app.use("/api", api);
 app.listen(PORT, () => console.log(`${PORT} 포트에서 서버 작동!!`));
